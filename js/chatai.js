98ccf3e5f2c6bd6e6c1267fca962c74753b433ba
primahagantha <primakaban61@gmail.com>
2023-10-31 12:25:14 +0700

    #fix API error CORS, adjust css

--- js/chatai.js
+++ js/chatai.js
@@ -2,66 +2,71 @@ document.addEventListener('DOMContentLoaded', function () {
     const inputName = document.getElementById('inputName');
     const inputAsk = document.getElementById('inputAsk');
     const buttonSubmit = document.querySelector('.button-submit');
-    const answerDiv = document.getElementById('answer');
     const balasanDiv = document.getElementById('balasan');
 
-
     buttonSubmit.addEventListener('click', function () {
-      const name = inputName.value.trim();
-      const question = inputAsk.value.trim();
-  
-      if (name === '' || question === '') {
-        showError('Nama dan pertanyaan harus diisi.');
-      } else {
-        showLoading();
-        const names = `Ayo Sapa si user ${name}, Aku Ingin Bertanya Kepada Kamu: ${question}` 
-        const apiUrl = 'https://tools.betabotz.org/tools/openai?q=' + encodeURI(names);
-  
-        axios.get(apiUrl)
-          .then(response => {
-            const answer = response.data.result;
-            simulateTyping(answer);
-          })
-          .catch(error => {
-            showError('Terjadi kesalahan dalam mengambil jawaban.');
-          });
-      }
-    });
-  
-    function simulateTyping(answer) {
-      let i = 0;
-      const typingInterval = setInterval(function () {
-        if (i <= answer.length) {
-          answerDiv.innerHTML = answer.substring(0, i);
-          i++;
+        const name = inputName.value.trim();
+        const question = inputAsk.value.trim();
+
+        if (name === '' || question === '') {
+            showError('Nama dan pertanyaan harus diisi.');
         } else {
-          clearInterval(typingInterval);
-          showResetButton();
+            showLoading();
+            const names = `Ayo Sapa si user ${name} (walau nama aneh / ngasal, tetap saja sapa namanya!), Aku Ingin Bertanya Kepada Kamu: ${question}`;
+            const apiUrl = 'http://ai.primahaganta.my.id:25678/api/openai?q=' + encodeURI(names);
+            const errorTimeout = setTimeout(() => {
+                showError('Waktu permintaan habis. Silakan coba lagi.');
+            }, 60000);
+
+            axios.get(apiUrl)
+                .then(response => {
+                    clearTimeout(errorTimeout)
+                    const answer = response.data.result;
+                    showAnswer(answer);
+                })
+                .catch(error => {
+                    clearTimeout(errorTimeout);
+                    showError('Terjadi kesalahan dalam mengambil jawaban.');
+                });
         }
-      }, 100);
+    });
+
+    function showAnswer(answer) {
+        let i = 0;
+        const addText = () => {
+            if (i <= answer.length) {
+                balasanDiv.innerHTML = answer.substring(0, i);
+                i++;
+                setTimeout(addText, 30); 
+            } else {
+                showResetButton();
+            }
+        };
+    
+        addText();
     }
-  
+    
+
     function showLoading() {
-      const loadingIcon = document.createElement('div');
-      loadingIcon.classList.add('loading');
-      balasanDiv.innerHTML = '';
-      balasanDiv.appendChild(loadingIcon);
+        const loadingIcon = document.createElement('div');
+        loadingIcon.classList.add('loading');
+        balasanDiv.innerHTML = '';
+        balasanDiv.appendChild(loadingIcon);
     }
-  
+
     function showError(message) {
-      balasanDiv.innerHTML = `<div class="border border-danger p-2 mb-2">${message}</div>`;
+        balasanDiv.innerHTML = `<div class="border border-danger p-2 mb-2">${message}</div>`;
     }
-  
+
     function showResetButton() {
-      const resetButton = document.createElement('button');
-      resetButton.innerText = 'Reset';
-      resetButton.classList.add('btn', 'btn-secondary', 'mt-3');
-      resetButton.addEventListener('click', function () {
-        inputName.value = '';
-        inputAsk.value = '';
-        answerDiv.innerHTML = '<div class="border border-success p-2 mb-2">.</div>';
-      });
-      balasanDiv.appendChild(resetButton);
+        const resetButton = document.createElement('button');
+        resetButton.innerText = 'Reset';
+        resetButton.classList.add('btn', 'btn-secondary', 'mt-3', 'd-flex', 'justify-content-sm-center');
+        resetButton.addEventListener('click', function () {
+            inputName.value = '';
+            inputAsk.value = '';
+            balasanDiv.innerHTML = '<div class="border border-success p-2 mb-2">.</div>';
+        });
+        balasanDiv.appendChild(resetButton);
     }
-  });
-  
+});
