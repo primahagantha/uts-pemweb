b4882fcb666c3fbc7c3169234ab19dd742f49136
primahagantha <primakaban61@gmail.com>
2023-10-31 19:53:45 +0700

    #Adjust Home page more better

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
-const parallax = document.querySelector('.parallax');
+let arrayHome = ["halo", "nǐ hǎo", "hola", "hello", "bonjour", "hallo", "ciao", "olá", "hej", "hei", "tungjatjeta", "marhaba", "salam", "kaixo", "demat", "zdraveĭte", "hodi", "bonghjornu", "bok", "ahoj", "goddag", "dobry dzien", "miesalmebi", "helló", "dia dhuit", "안녕하세요", "tere", "kamusta", "hyvää päivää", "שלום", "नमस्ते", "sveiki", "bongu", "नमस्ते", "سلام", "zdravo", "χαίρε", "sabaidi", "salve", "talofa", "mhoro", "szia", "chào bạn", "helo", "hela", "sawubona"];
+const typeDiv = document.getElementById("type-home");
+let timer;
 
-window.addEventListener('scroll', () => {
-  let scrollPosition = window.scrollY;
-  parallax.style.backgroundPositionY = scrollPosition * 0.7 + 'px';
-});
+function frameLooper() {
+  if (arrayHome.length > 0) {
+    timer = setInterval(() => {
+      let randomIndex = Math.floor(Math.random() * arrayHome.length);
+      typeDiv.textContent = arrayHome[randomIndex];
+      arrayHome.splice(randomIndex, 1);
+
+      if (arrayHome.length === 0) {
+        clearInterval(timer);
+      }
+    }, 1000); // 1000ms interval
+  }
+}
+
+frameLooper();
